b9bf2515eccc2b54b9cf9be38907272063dd4fc1
Manuel <manu.gallucci@gmail.com>
2022-01-04 12:50:40 +0100

    Commit intermedio

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,49 +1,105 @@
 import { useEffect, useState } from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Web3 from 'web3';
-import { CONTACT_ABI, CONTACT_ADDRESS } from './config';
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from './config';
 
 function App() {
-	const [account, setAccount] = useState();
-	const [contactList, setContactList] = useState();
-	const [contacts, setContacts] = useState([]);
+	const [account, setAccountState] = useState();
+	const [myContract, setmyContractState] = useState();
+	const [balanceToken, setBalanceTokenState] = useState();
+	//const [contacts, setContacts] = useState([]);
 
 	useEffect(() => {
 		async function load() {
-			const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
-	    const accounts = await web3.eth.requestAccounts();
-	    setAccount(accounts[0]);
+			//collegamento alla blockchain tramite metamask
+			const web3 = new Web3(Web3.givenProvider /* || 'http://localhost:7545' */);
 
-	    const contactList = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
+			//assegniamo alla variabile account l'account attuamente selezionato in metamask
+			const accounts = await web3.eth.requestAccounts();
+			setAccountState(accounts[0]);
 
-	    setContactList(contactList);
+			//assegniamo alla variabile myContract il riferimento al contratto
+			const myContractResponse = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+			setmyContractState(myContractResponse);
 
-	    const counter = await contactList.methods.count().call();
+			const balanceTokenResponse = await myContractResponse.methods.balanceOf(accounts[0]).call();
+			const balanceTokenResponseConverted = web3.utils.fromWei(balanceTokenResponse);
+			setBalanceTokenState(balanceTokenResponseConverted);
 
-	    for (var i = 1; i <= counter; i++) {
-	      const contact = await contactList.methods.contacts(i).call();
-	      setContacts((contacts) => [...contacts, contact]);
-	    }
+			//riassegniamo alla variabile account, il nuovo account selezionato in metamask dall'utente
+			window.ethereum.on('accountsChanged', async function (accounts) {
+				setAccountState(accounts[0]);
+				const balanceTokenResponse = await myContractResponse.methods.balanceOf(accounts[0]).call();
+				const balanceTokenResponseConverted = web3.utils.fromWei(balanceTokenResponse);
+				setBalanceTokenState(balanceTokenResponseConverted);
+			});
+
+			/*
+			const counter = await myContract.methods.count().call();
+
+			for (var i = 1; i <= counter; i++) {
+				const contact = await myContract.methods.contacts(i).call();
+				setContacts((contacts) => [...contacts, contact]);
+			}
+			*/
+
 		}
 
 		load();
 	}, []);
 
-  return (
-    <div>
-    	Your account is: {account}
-    	<h1>Contacts</h1>
-    	<ul>
-    	{
-    		Object.keys(contacts).map((contact, index) => (
-					<li key={`${contacts[index].name}-${index}`}>
-						<h4>{contacts[index].name}</h4>
-						<span><b>Phone: </b>{contacts[index].phone}</span>
-					</li>
-				))
-    	}
-    	</ul>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar bg="dark" variant="dark">
+					<Container>
+						<Navbar.Brand href="#home">
+							<img
+								alt=""
+								src="/logo.svg"
+								width="30"
+								height="30"
+								className="d-inline-block align-top"
+							/>{' '}
+							React Bootstrap
+						</Navbar.Brand>
+					</Container>
+				</Navbar>
+				
+			<Container>
+				<Row>
+					<Col><Button variant="primary">Primary</Button>{' '}</Col>
+					<Col><Card>
+						<Card.Img variant="top" src="older.js/100px160" />
+						<Card.Body>
+							<Card.Title>Card title</Card.Title>
+							<Card.Text>
+								This is a wider card with supporting text below as a natural lead-in to
+								additional content. This content is a little bit longer.
+							</Card.Text>
+						</Card.Body>
+						<Card.Footer>
+							<small className="text-muted">Last updated 3 mins ago</small>
+						</Card.Footer>
+					</Card></Col>
+				</Row>
+				<Row>
+					<Col><p>Your account is: {account}</p></Col>
+					<Col><p>Your balance is: {balanceToken}</p></Col>
+					<Col>3 of 3</Col>
+				</Row>
+			</Container>
+		</div>
+				
+	);
 }
 
 export default App;
--- contacts/src/config.js
+++ contacts/src/config.js
@@ -1,73 +1,526 @@
-export const CONTACT_ADDRESS = '0xfAd567EBdCb36f49F3a509FEDF9e72E3ad75ca59'
+export const CONTRACT_ADDRESS = '0xb685B6f623c7F7b0E3295fFB4e98d33E0fDaF267'
 
-export const CONTACT_ABI = [
+export const CONTRACT_ABI = [
   {
-    "constant": true,
-    "inputs": [],
-    "name": "count",
-    "outputs": [
-      {
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function",
-    "signature": "0x06661abd"
-  },
-  {
-    "constant": true,
     "inputs": [
       {
-        "name": "",
+        "internalType": "uint256",
+        "name": "_total_supply",
         "type": "uint256"
       }
     ],
-    "name": "contacts",
-    "outputs": [
-      {
-        "name": "id",
-        "type": "uint256"
-      },
-      {
-        "name": "name",
-        "type": "string"
-      },
-      {
-        "name": "phone",
-        "type": "string"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function",
-    "signature": "0xe0f478cb"
-  },
-  {
-    "inputs": [],
-    "payable": false,
     "stateMutability": "nonpayable",
-    "type": "constructor",
-    "signature": "constructor"
+    "type": "constructor"
   },
   {
-    "constant": false,
+    "anonymous": false,
     "inputs": [
       {
-        "name": "_name",
-        "type": "string"
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
       },
       {
-        "name": "_phone",
+        "indexed": true,
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      }
+    ],
+    "name": "allowance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "subtractedValue",
+        "type": "uint256"
+      }
+    ],
+    "name": "decreaseAllowance",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "addedValue",
+        "type": "uint256"
+      }
+    ],
+    "name": "increaseAllowance",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
         "type": "string"
       }
     ],
-    "name": "createContact",
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "renounceOwnership",
     "outputs": [],
-    "payable": false,
     "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
     "type": "function",
-    "signature": "0x3dce4920"
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "recipient",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "sender",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "recipient",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "registerAddress",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "deregisterAddress",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "verifyRegistration",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string[]",
+        "name": "_jsonArray",
+        "type": "string[]"
+      }
+    ],
+    "name": "_generateIntermediateMerkleRoots",
+    "outputs": [
+      {
+        "internalType": "bytes32",
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_selector",
+        "type": "string"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "_intermediateRoot",
+        "type": "bytes32"
+      }
+    ],
+    "name": "_publishIntermediateMerkleRoots",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_address",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "_selector",
+        "type": "string"
+      }
+    ],
+    "name": "_getIntermediateMerkleRoots",
+    "outputs": [
+      {
+        "internalType": "bytes32[]",
+        "name": "",
+        "type": "bytes32[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_address",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "_selector",
+        "type": "string"
+      }
+    ],
+    "name": "_publishFinalMerkleRoot",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_address",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "_selector",
+        "type": "string"
+      }
+    ],
+    "name": "_getFinalMerkleRoot",
+    "outputs": [
+      {
+        "internalType": "bytes32",
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_address",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "_selector",
+        "type": "string"
+      },
+      {
+        "internalType": "bytes32[]",
+        "name": "_temporaryIntermediateMerkleRoots",
+        "type": "bytes32[]"
+      }
+    ],
+    "name": "_verifyFinalMerkleRoot",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
   }
 ];
